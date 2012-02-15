dcfde008dad087f59f10562a710b41bf5915d4bb
Luka Perkov <freecwmp@lukaperkov.net>
2012-02-15 22:00:53 +0100

    downgrade the license to GPLv2

--- src/xml/xml.h
+++ src/xml/xml.h
@@ -1,7 +1,7 @@
 /*
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
+ *	the Free Software Foundation, either version 2 of the License, or
  *	(at your option) any later version.
  *
  *	Copyright (C) 2011 Luka Perkov <freecwmp@lukaperkov.net>
