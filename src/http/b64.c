bafbd7783ac3e8cb06a5a1848ecf27509a50d965
Luka Perkov <freecwmp@lukaperkov.net>
2012-02-27 00:48:04 +0100

    fix license for code taken from zstream

--- src/http/b64.c
+++ src/http/b64.c
@@ -1,16 +1,21 @@
 /*
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 2 of the License, or
- *	(at your option) any later version.
- *
- *	Copyright (C) 2012 Luka Perkov <freecwmp@lukaperkov.net>
- *
- *	This code is taken from zstream.
- *
  *	zstream - Micro URL I/O stream library
  *	Copyright (C) 2010 Steven Barth <steven@midlink.org>
  *	Copyright (C) 2010 John Crispin <blogic@openwrt.org>
+ *
+ *	This library is free software; you can redistribute it and/or modify it
+ *	under the terms of the GNU Lesser General Public License as published
+ *	by the Free Software Foundation; either version 2.1 of the License,
+ *	or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *	See the GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public License
+ *	along with this library; if not, write to the Free Software Foundation,
+ *	Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  *
  */
 
